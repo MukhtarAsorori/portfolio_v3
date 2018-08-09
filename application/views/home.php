f5fe8cf75451b9907cf7233a4704ebb133fd8401
nfraz007 <nfraz007@gmail.com>
2018-08-10 00:55:09 +0530

    almost complete home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,21 +1,173 @@
-<div class="container">
-	<div class="row">
-		<div class="col-sm-6">
-			<h2>Nazish</h2>
-		</div>
-		<div class="col-sm-6">
-			<h2>Nazish</h2>
+<div id="page_home">
+	<div class="gap"></div>
+	<div class="container section_bio">
+		<div class="row">
+			<div class="col-sm-6">
+				<h2>I am Nazish Fraz</h2>
+				<h3>Full Stack developer</h3>
+				<p>I am a Web Developer located in Hartford, Connecticut. I currently work as a part time Remote Junior Web Developer for Coolor, located in Las Vegas. I am looking to take on more work and to increase my skills as a Web Developer.</p>
+				<div class="gap-small"></div>
+				<div class="row">
+					<div class="col-xs-3">
+						<p><b>Phone</b></p>
+						<p><b>Email</b></p>
+						<p><b>Address</b></p>
+					</div>
+					<div class="col-xs-9">
+						<p>9748277144</p>
+						<p>nfraz007@gmail.com</p>
+						<p>Bengaluru, India</p>
+					</div>
+				</div>
+				<div class="gap-small"></div>
+				<div class="row">
+					<div class="col-xs-2">
+						<a href="#"><i class="fa fa-linkedin my_btn_icon text-center"></i></a>
+					</div>
+					<div class="col-xs-2">
+						<a href="#"><i class="fa fa-github my_btn_icon text-center"></i></a>
+					</div>
+					<div class="col-xs-2">
+						<a href="#"><i class="fa fa-medium my_btn_icon text-center"></i></a>
+					</div>
+					<div class="col-xs-2">
+						<a href="#"><i class="fa fa-codepen my_btn_icon text-center"></i></a>
+					</div>
+					<div class="col-xs-2">
+						<a href="#"><i class="fa fa-angellist my_btn_icon text-center"></i></a>
+					</div>
+					<div class="col-xs-2">
+						<a href="#"><i class="fa fa-quora my_btn_icon text-center"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<img src="<?php echo asset_url();?>/images/myphoto.jpg" alt="" class="img-responsive pull-right">
+			</div>
 		</div>
 	</div>
-</div>
-<hr>
-<div class="container">
-	<div class="row">
-		<div class="col-sm-6">
-			<h2>Nazish</h2>
-		</div>
-		<div class="col-sm-6">
-			<h2>Nazish</h2>
+	<div class="gap"></div>
+	<hr>
+	<div class="gap"></div>
+	<div class="container section_skill">
+		<div class="row">
+			<div class="col-sm-4">
+				<h2>Skills</h2>
+				<p>I am inspired by creating great work with people who are as passionate as I am about building something awesome.</p>
+			</div>
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-sm-6">
+						<h4>Web Development</h4>
+						<p>Javascript, Coffeescript, JAVA, Python, PHP, Go, Node.js, MongoDB, Redis, PostgreSQL, WebStorm.</p>
+						<div class="gap-small"></div>
+					</div>
+					<div class="col-sm-6">
+						<h4>Web Development</h4>
+						<p>Javascript, Coffeescript, JAVA, Python, PHP, Go, Node.js, MongoDB, Redis, PostgreSQL, WebStorm.</p>
+						<div class="gap-small"></div>
+					</div>
+					<div class="col-sm-6">
+						<h4>Web Development</h4>
+						<p>Javascript, Coffeescript, JAVA, Python, PHP, Go, Node.js, MongoDB, Redis, PostgreSQL, WebStorm.</p>
+						<div class="gap-small"></div>
+					</div>
+					<div class="col-sm-6">
+						<h4>Web Development</h4>
+						<p>Javascript, Coffeescript, JAVA, Python, PHP, Go, Node.js, MongoDB, Redis, PostgreSQL, WebStorm.</p>
+						<div class="gap-small"></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
+	<div class="gap"></div>
+	<hr>
+	<div class="gap"></div>
+	<div class="container section_experience">
+		<div class="row">
+			<div class="col-sm-4">
+				<h2>Experience</h2>
+				<p>I partner with startups, organizations and Fortune 500 companies to build digital products that help clients overcome challenges and create lasting connections with millions of people every day.</p>
+			</div>
+			<div class="col-sm-8">
+				<div>
+					<h4>Full Stack Developer</h4>
+					<h4 class="color_text_grey_dark normal">MildlyClassic Pvt. Ltd</h4>
+					<h5 class="color_text_grey_dark">Jan 2018 - Present <i class="fa fa-circle"></i> 8 Months</h5>
+					<p>MildlyClassic is a startup which is a service and product based company. right now sequel logistic is the biggest client of mildlyClassic. We are building the entire ERP system for this company. Sequel has lots of big client like Titan, Khazana, and many Jewelry companies. ERP is in Codeigniter framework and we are handling huge traffic from entire country and maintaining its scalability and speed services.</p>
+					<hr>
+				</div>
+				<div>
+					<h4>Web Developer</h4>
+					<h4 class="color_text_grey_dark normal">Floret Media Pvt. Ltd</h4>
+					<h5 class="color_text_grey_dark">Jul 2017 - Jan 2018 <i class="fa fa-circle"></i> 7 Months</h5>
+					<p>Floret media is a software company, and its biggest product is pepagora. I am currently doing this project. Pepagora is a b2b marketplace for supplier and manufacturer. I am adding some new features into this. Parallelly I am creating an Api for this in lumen because its mobile application is coming to soon. The pepagora website is in core php and codeigniter framework. As I mentioned, floret is a software company so there are lots of client project as well. According to the need I am also working on some clients project like kuberansilks, ayyan fireworks, masonlite, 1mg, haute-horologe etc.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="gap"></div>
+	<hr>
+	<div class="gap"></div>
+	<div class="container section_education">
+		<div class="row">
+			<div class="col-sm-4">
+				<h2>Education</h2>
+				<p>All my life I have been driven by my strong belief that education is important. I try to learn something new every single day.</p>
+			</div>
+			<div class="col-sm-8">
+				<div>
+					<h4>West Bengal University of Technology</h4>
+					<h5 class="color_text_grey_dark">2013 - 2017</h5>
+					<p>Bachelor of Technology (B.Tech), Information Technology, 7.4</p>
+					<hr>
+				</div>
+				<div>
+					<h4>MK DAV Public School</h4>
+					<h5 class="color_text_grey_dark">2012</h5>
+					<p>12th, CBSE, 68%</p>
+					<hr>
+				</div>
+				<div>
+					<h4>Rotary School</h4>
+					<h5 class="color_text_grey_dark">2010</h5>
+					<p>10th, CBSE, 8.6</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="gap"></div>
+	<hr>
+	<div class="gap"></div>
+	<div class="container section_education">
+		<div class="row">
+			<div class="col-sm-4">
+				<h2>Achievements</h2>
+				<p>All my life I have been driven by my strong belief that education is important. I try to learn something new every single day.</p>
+			</div>
+			<div class="col-sm-8">
+				<div>
+					<p><i class="fa fa-hand-o-right"></i> 23rd rank global in CodeVita 2016, 1 st round organised by TCS.</p>
+					<hr>
+				</div>
+				<div>
+					<p><i class="fa fa-hand-o-right"></i> 23rd rank global in CodeVita 2016, 1 st round organised by TCS.</p>
+					<hr>
+				</div>
+				<div>
+					<p><i class="fa fa-hand-o-right"></i> 23rd rank global in CodeVita 2016, 1 st round organised by TCS.</p>
+					<hr>
+				</div>
+				<div>
+					<p><i class="fa fa-hand-o-right"></i> 23rd rank global in CodeVita 2016, 1 st round organised by TCS.</p>
+					<hr>
+				</div>
+				<div>
+					<p><i class="fa fa-hand-o-right"></i> 23rd rank global in CodeVita 2016, 1 st round organised by TCS.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="gap"></div>
 </div>
--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,3 +1,24 @@
+	<footer id="footer">
+		<div class="gap"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<a href="#" class="btn-block color_text_white">About Me</a>
+					<a href="#" class="btn-block color_text_white">Work Experience</a>
+					<a href="#" class="btn-block color_text_white">Projects</a>
+					<a href="#" class="btn-block color_text_white">Certifications</a>
+					<a href="#" class="btn-block color_text_white">Blog</a>
+				</div>
+				<div class="col-sm-4">
+					<p>sdssdsd</p>
+				</div>
+				<div class="col-sm-4">
+					<p>sj djhd sjd sjdk jks d</p>
+				</div>
+			</div>
+		</div>
+		<div class="gap"></div>
+	</footer>
 </body>
 </html>
 <?php $this->load->view("include/js");?>
