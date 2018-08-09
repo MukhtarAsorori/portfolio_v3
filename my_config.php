964f5c600690b3336e3ffe8dae74e1346b8ee5d0
nfraz007 <nfraz007@gmail.com>
2018-08-10 00:58:47 +0530

    added my_config.php

---
+++ my_config.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE CUSTOM FILE CONFIGURATIONS
+ * --------------------------------------------------------------------
+ *
+ */
+
+/**
+ * Configuration for base URL
+ */
+$BASE_URL = "http://localhost/nazish/portfolio_v3";
+// $BASE_URL = "http://192.168.43.35/nazish/portfolio_v3";
+$GLOBALS['BASE_URL'] = $BASE_URL;
+
+/**
+ * Configuration for database
+ */
+$database = array(
+	'HOSTNAME' 			=> 'localhost',
+	'USERNAME' 			=> 'root',
+	'PASSWORD' 			=> 'root',
+	'DATABASE_NAME' 	=> 'portfolio_v3',
+);
+$GLOBALS['DATABASE'] = $database;
+
+$GLOBALS['maintenance_mode'] 	= FALSE;
+$GLOBALS["ENCRYPTION_KEY"] = "";
+?>
